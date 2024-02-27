92a76043ec9c8dda12e8a1081e42471c18a5d004
jafneyjoseph <jarcelinjafney28@gmail.com>
2024-02-27 18:27:43 +0530

    added

--- travelproject/travelapp/urls.py
+++ travelproject/travelapp/urls.py
@@ -3,9 +3,6 @@ from django.urls import path
 
 urlpatterns = [
 
-    # path('places/', views.places, name='places'),
-    # path('teams/', views.teams, name='teams'),
     path('', views.places, name='places'),
-    path('teams/', views.teams, name='teams'),
 
 ]
